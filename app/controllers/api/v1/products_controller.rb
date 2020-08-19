8d4d2b3a95fa9cbc4e087a03151da0e2391c8148
mubarik <mubarik.shad@gmail.com>
2020-08-19 05:15:01 -0400

    status added

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,17 +2,19 @@ module Api
 	module V1
 		class ProductsController < ApplicationController
 			def index
-				products = Product.includes(:promotions)
+				products = Product.joins(:promotions).where(promotions: { status: "active"} ).pluck("products.*, promotions.discount")
 				render json: {status: 'SUCCESS', message: 'Loaded articles', data: products}, status: :ok
 			end
 
 			def by_department
-				data = params[:department]
+				department = params[:department]
+				data = Department.find_by!(name: department).products
 				render json: {status: 'SUCCESS', message: 'Loaded articles', data: data}, status: :ok
 			end
 
 			def by_promotion
-				data = params[:promotion]
+				code = params[:code]
+				data = Product.joins(:promotions).where(promotions: { code: code} ).pluck("products.*, promotions.discount")
 				render json: {status: 'SUCCESS', message: 'Loaded articles', data: data}, status: :ok
 			end 
 		end
